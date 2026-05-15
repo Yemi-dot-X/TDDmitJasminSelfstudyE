ea1176191a20acb9e7c42757e65f315aa35a268d
Yemi Akinrinlola <if25b072@technikum-wien.at>
2026-05-15 08:33:54 +0200

    add support for comma separated numbers

--- string_calculator/spec/string_calculator_spec.js
+++ string_calculator/spec/string_calculator_spec.js
@@ -2,12 +2,9 @@ describe('String Calculator', function() {
 	beforeEach(function() {
 		calculator = new StringCalculator();
 	});
+	
 
-	 it("should return 0 for empty string", function () {
-        expect(calculator.add("")).toEqual(0);
-    });
-
-	it('should return 1 for one number', function() {
-		expect(calculator.add('1')).toEqual(1);
-	});
+	it('should add two comma separated numbers', function() {
+    expect(calculator.add('1,2')).toEqual(3);
+});
 });
--- string_calculator/src/string_calculator.js
+++ string_calculator/src/string_calculator.js
@@ -2,11 +2,16 @@
 function StringCalculator() {}
 StringCalculator.prototype.add = function(string_numbers) {
 
-	if (string_numbers === "") {
+	 if (string_numbers === '') {
         return 0;
     }
 
-	if(string_numbers === '1'){
-		return 1;
-	}
+    var numbers = string_numbers.split(',');
+    var sum = 0;
+
+    for (var i = 0; i < numbers.length; i++) {
+        sum += parseInt(numbers[i]);
+    }
+
+    return sum;
 };
