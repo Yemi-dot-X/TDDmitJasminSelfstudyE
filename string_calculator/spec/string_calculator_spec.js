441526369ff3dfcb649292be93b7cbddd9e838d8
Yemi Akinrinlola <if25b072@technikum-wien.at>
2026-05-15 08:36:27 +0200

    support empty string one string and three strings

--- string_calculator/spec/string_calculator_spec.js
+++ string_calculator/spec/string_calculator_spec.js
@@ -2,7 +2,14 @@ describe('String Calculator', function() {
 	beforeEach(function() {
 		calculator = new StringCalculator();
 	});
-	
+
+	it('should return 0 for an empty string', function() {
+		expect(calculator.add('')).toEqual(0);
+	});
+
+		it('should return 1 for an 1 string', function() {
+		expect(calculator.add('')).toEqual(0);
+	});
 
 	it('should add two comma separated numbers', function() {
     expect(calculator.add('1,2')).toEqual(3);
