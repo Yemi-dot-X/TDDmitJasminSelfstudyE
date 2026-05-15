e553c02f1501eb691ccb9bbc8cf9d3f71d24a960
Yemi Akinrinlola <if25b072@technikum-wien.at>
2026-05-15 08:51:29 +0200

    test case5

--- string_calculator/src/string_calculator.js
+++ string_calculator/src/string_calculator.js
@@ -2,15 +2,10 @@
 function StringCalculator() {}
 StringCalculator.prototype.add = function(string_numbers) {
 
-		if(string_numbers == ""){
-			return 0;
-		}
 		
 		const numbers = string_numbers.split(",");
 
-		if(numbers.length === 1){
-			return parseInt(numbers[0]);
-		}
 		
+
 	    return parseInt(numbers[0]) + parseInt(numbers[1]);
 };
