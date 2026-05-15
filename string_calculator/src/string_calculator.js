4f9bf12ae8f87cd1c894f55539ce153eb37bfa86
Yemi Akinrinlola <if25b072@technikum-wien.at>
2026-05-15 08:49:15 +0200

    refactor case one two three four

--- string_calculator/src/string_calculator.js
+++ string_calculator/src/string_calculator.js
@@ -1,6 +1,16 @@
 // StringCalculator.js
 function StringCalculator() {}
 StringCalculator.prototype.add = function(string_numbers) {
+
+		if(string_numbers == ""){
+			return 0;
+		}
+		
 		const numbers = string_numbers.split(",");
+
+		if(numbers.length === 1){
+			return parseInt(numbers[0]);
+		}
+		
 	    return parseInt(numbers[0]) + parseInt(numbers[1]);
 };
