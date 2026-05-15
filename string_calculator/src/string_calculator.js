06fdc69524f5eb75be7b4d9452e550f66a463ce3
Yemi Akinrinlola <if25b072@technikum-wien.at>
2026-05-15 08:31:09 +0200

    refactor should return 0 and 1

--- string_calculator/src/string_calculator.js
+++ string_calculator/src/string_calculator.js
@@ -1,6 +1,11 @@
 // StringCalculator.js
 function StringCalculator() {}
 StringCalculator.prototype.add = function(string_numbers) {
+
+	if (string_numbers === "") {
+        return 0;
+    }
+
 	if(string_numbers === '1'){
 		return 1;
 	}
